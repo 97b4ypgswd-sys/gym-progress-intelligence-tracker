a2f564485d1a8abdf4765aa5feb56299baa798eb
Anshika <anshika@Anshikas-MacBook-Air.local>
2026-05-31 19:58:52 +0530

    Added User Input

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print ("Hello GitHub")
-print ("My name is Anshika")
+name = input("Enter your name: ")
+weight = float(input("Enter your weight in kg: "))
+
+print("Name:", name)
+print("Weight:", weight)
